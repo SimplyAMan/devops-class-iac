9c6f4951eef19e1dc767d9a5b4d9cf0e320bc3f5
SimplyAMan <simplyaman@gmail.com>
2021-11-21 13:04:12 +0200

    Refactoring

--- terraform/netword.tf
+++ terraform/netword.tf
@@ -1,94 +1,94 @@
-resource "aws_vpc" "exam-vpc" {
+resource "aws_vpc" "lesson12-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
   instance_tenancy     = "default"
 
   tags = {
-    "Name" = "exam-vpc"
+    "Name" = "lesson12-vpc"
   }
 }
 
-resource "aws_subnet" "exam-ec2-subnet" {
-  vpc_id                  = aws_vpc.exam-vpc.id
+resource "aws_subnet" "lesson12-ec2-subnet" {
+  vpc_id                  = aws_vpc.lesson12-vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1a"
 
   tags = {
-    "Name" = "exam-ec2-subnet"
+    "Name" = "lesson12-ec2-subnet"
   }
 }
 
-resource "aws_subnet" "exam-rds-subnet1" {
-  vpc_id                  = aws_vpc.exam-vpc.id
+resource "aws_subnet" "lesson12-rds-subnet1" {
+  vpc_id                  = aws_vpc.lesson12-vpc.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1b"
 
   tags = {
-    "Name" = "exam-rds-subnet1"
+    "Name" = "lesson12-rds-subnet1"
   }
 }
 
-resource "aws_subnet" "exam-rds-subnet2" {
-  vpc_id                  = aws_vpc.exam-vpc.id
+resource "aws_subnet" "lesson12-rds-subnet2" {
+  vpc_id                  = aws_vpc.lesson12-vpc.id
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = true
   availability_zone       = "eu-central-1c"
 
   tags = {
-    "Name" = "exam-rds-subnet1"
+    "Name" = "lesson12-rds-subnet1"
   }
 }
 
-resource "aws_db_subnet_group" "exam-rds-subnet-group" {
+resource "aws_db_subnet_group" "lesson12-rds-subnet-group" {
   name       = "rds subnet group"
-  subnet_ids = ["${aws_subnet.exam-rds-subnet1.id}", "${aws_subnet.exam-rds-subnet2.id}"]
+  subnet_ids = ["${aws_subnet.lesson12-rds-subnet1.id}", "${aws_subnet.lesson12-rds-subnet2.id}"]
 
   tags = {
-    "Name" = "exam-rds-subnet-group"
+    "Name" = "lesson12-rds-subnet-group"
   }
 }
 
-resource "aws_internet_gateway" "exam-igw" {
-    vpc_id = aws_vpc.exam-vpc.id
+resource "aws_internet_gateway" "lesson12-igw" {
+    vpc_id = aws_vpc.lesson12-vpc.id
 
     tags = {
-      "Name" = "exam-igw"
+      "Name" = "lesson12-igw"
     }
 }
 
-resource "aws_route_table" "exam-public-crt" {
-    vpc_id = aws_vpc.exam-vpc.id
+resource "aws_route_table" "lesson12-public-crt" {
+    vpc_id = aws_vpc.lesson12-vpc.id
     
     route {
         cidr_block = "0.0.0.0/0" 
-        gateway_id = aws_internet_gateway.exam-igw.id
+        gateway_id = aws_internet_gateway.lesson12-igw.id
     }
     
     tags = {
-        Name = "exam-public-crt"
+        Name = "lesson12-public-crt"
     }
 }
 
-resource "aws_route_table_association" "exam-crta-public-ec2-subnet" {
-  subnet_id      = aws_subnet.exam-ec2-subnet.id
-  route_table_id = aws_route_table.exam-public-crt.id
+resource "aws_route_table_association" "lesson12-crta-public-ec2-subnet" {
+  subnet_id      = aws_subnet.lesson12-ec2-subnet.id
+  route_table_id = aws_route_table.lesson12-public-crt.id
 }
 
-resource "aws_route_table_association" "exam-crta-public-rds-subnet-1" {
-  subnet_id      = aws_subnet.exam-rds-subnet1.id
-  route_table_id = aws_route_table.exam-public-crt.id
+resource "aws_route_table_association" "lesson12-crta-public-rds-subnet-1" {
+  subnet_id      = aws_subnet.lesson12-rds-subnet1.id
+  route_table_id = aws_route_table.lesson12-public-crt.id
 }
 
-resource "aws_route_table_association" "exam-crta-public-rds-subnet-2" {
-  subnet_id      = aws_subnet.exam-rds-subnet2.id
-  route_table_id = aws_route_table.exam-public-crt.id
+resource "aws_route_table_association" "lesson12-crta-public-rds-subnet-2" {
+  subnet_id      = aws_subnet.lesson12-rds-subnet2.id
+  route_table_id = aws_route_table.lesson12-public-crt.id
 }
 
-resource "aws_security_group" "sg-exam-all" {
-  vpc_id = aws_vpc.exam-vpc.id
+resource "aws_security_group" "sg-lesson12-all" {
+  vpc_id = aws_vpc.lesson12-vpc.id
 
   egress {
     from_port   = 0
@@ -104,6 +104,6 @@ resource "aws_security_group" "sg-exam-all" {
   }
 
   tags = {
-    "Name" = "sg-exam-all"
+    "Name" = "sg-lesson12-all"
   }
 }
