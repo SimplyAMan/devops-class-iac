9ccee845ebc7d92f6f76e696a3bcddf4622fe4f7
SimplyAMan <simplyaman@gmail.com>
2021-11-21 21:26:55 +0200

    Add apache

---
+++ terraform/apache.tf
@@ -0,0 +1,13 @@
+# Create an EC2 instance
+resource "aws_instance" "lesson12-apache" {
+  ami           = "ami-0a49b025fffbbdac6"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.lesson12-ec2-subnet.id
+  vpc_security_group_ids = ["${aws_security_group.sg-lesson12-all.id}"]
+
+  key_name = "aws-key"
+
+  tags = {
+    Name = "lesson12-apache"
+  }
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,15 +3,12 @@
 #   value       = aws_db_instance.lesson12-rds.endpoint
 # }
 
-# output "eks_endpoint" {
-#   value = data.aws_eks_cluster.cluster.endpoint
-# }
-
-# output "kubeconfig-certificate-authority-data" {
-#   value = data.aws_eks_cluster.cluster.certificate_authority[0].data
-# }
-
 output "ec2_endpoint" {
   description = "The address of the EC2 instance"
   value       = aws_instance.lesson12-ec2.public_ip
+}
+
+output "apache_endpoint" {
+  description = "The address of the Apache instance"
+  value = aws_instance.lesson12-apache.public_ip
 }
