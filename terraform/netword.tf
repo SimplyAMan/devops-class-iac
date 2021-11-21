68ff959094654be4660744cd37dcbd2edeadb28b
SimplyAMan <simplyaman@gmail.com>
2021-11-21 19:37:01 +0200

    Changed security group

--- terraform/netword.tf
+++ terraform/netword.tf
@@ -90,18 +90,39 @@ resource "aws_route_table_association" "lesson12-crta-public-rds-subnet-2" {
 resource "aws_security_group" "sg-lesson12-all" {
   vpc_id = aws_vpc.lesson12-vpc.id
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  # egress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = -1
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+  # ingress {
+  #   from_port   = 0
+  #   to_port     = 0
+  #   protocol    = -1
+  #   cidr_blocks = ["0.0.0.0/0"]
+  # }
+
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
+    from_port = "80"
+    to_port = "80"
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
 
   tags = {
     "Name" = "sg-lesson12-all"
